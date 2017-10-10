a6cb5bf1d6ba044bd25073e0177919694838ff26
Joe Tsai <joetsai@digital-static.net>
2017-10-10 15:46:58 -0700

    Log jsonfmt.Format error

--- main.go
+++ main.go
@@ -131,7 +131,9 @@ func loadConfig(conf string) (tunns []tunnel, logger *log.Logger, closer func() 
 	if err != nil {
 		logger.Fatalf("unable to read config: %v", err)
 	}
-	c, _ = jsonfmt.Format(c, jsonfmt.Minify())
+	if c, err = jsonfmt.Format(c, jsonfmt.Standardize()); err != nil {
+		logger.Fatalf("unable to parse config: %v", err)
+	}
 	if err := json.Unmarshal(c, &config); err != nil {
 		logger.Fatalf("unable to decode config: %v", err)
 	}
